2ce82036fc3196b8427c030623184bd196f0bd03
Aosaf098 <aosafibad10@gmail.com>
2024-11-18 13:42:49 +0600

    Project Setup

---
+++ src/Firebase/Firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDlseQR9FBOO3BDIeNkPs2WPFrnrivOQHo",
+  authDomain: "discount-hunter-233c8.firebaseapp.com",
+  projectId: "discount-hunter-233c8",
+  storageBucket: "discount-hunter-233c8.firebasestorage.app",
+  messagingSenderId: "875043151725",
+  appId: "1:875043151725:web:7b0e6e7b1385e11d3d0464"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
---
+++ src/Router/routes.jsx
@@ -0,0 +1,12 @@
+import { createBrowserRouter } from "react-router-dom";
+
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <h1 className="text-7xl text-green-500">Home</h1>
+    }
+])
+
+
+export default router
